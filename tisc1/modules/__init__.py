de18834b564275a70943a374fdd56d036ea2044e
Dang Minh Quan <kingdaica25@gmail.com>
2024-06-26 18:47:31 +0700

    first commit

---
+++ tisc1/modules/__init__.py
@@ -0,0 +1,5 @@
+from .BiLSTM import BiLSTM
+from .LSTM import LSTM
+from .Transformer import Transformer
+from .utils import Builder, TiscNetwork, filter_kwargs_for_module
+from .head import BasicClassificationHead, IdentityHead
---
+++ tisc1/modules/utils/__init__.py
@@ -0,0 +1,1 @@
+from .builder import Builder, TiscNetwork, filter_kwargs_for_module
